454e92ff15c1b2df1fcb75f099436e4a1946fd02
Ger-B <germanignacio.bianco@estudiantes.unahur.edu.ar>
2025-06-02 23:18:40 -0300

    Implementa la interfaz programable en microprocesador y define sus metodos.

--- src/main/java/ar/edu/unahur/obj2/command/Microprocesador.java
+++ src/main/java/ar/edu/unahur/obj2/command/Microprocesador.java
@@ -1,5 +1,76 @@
 package ar.edu.unahur.obj2.command;
 
-public class Microprocesador {
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unahur.obj2.command.comandos.Operable;
+
+public class Microprocesador implements Programable{
+    private Integer acumA = 0;
+    private Integer acumB = 0;
+    private Integer pC = 0;
+    private List<Integer> datos = new ArrayList<>();
+
+    @Override
+    public void run(List<Operable> operaciones) {
+        operaciones.forEach(op -> op.execute(this));
+    }
+
+    @Override
+    public void incProgramCounter() {
+        pC += 1;
+    }
+
+    @Override
+    public Integer getProgramCounter() {
+        return pC;
+    }
+
+    @Override
+    public void setAcumuladorA(Integer value) {
+        acumA = value;
+    }
+
+    @Override
+    public Integer getAcumuladorA() {
+        return acumA;
+    }
+
+    @Override
+    public void setAcumuladorB(Integer value) {
+        acumB = value;
+    }
+
+    @Override
+    public Integer getAcumuladorB() {
+        return acumB;
+    }
+
+    @Override
+    public void copyFrom(Programable programable) {
+        //TODO
+    }
+
+    // @Override
+    // public Programable copy() {
+    //     return ;// TODO 
+    // }
+
+    @Override
+    public void reset() {
+        pC = 0;
+        acumA = 0;
+        acumB = 0;
+    }
+
+    @Override
+    public void setAddr(Integer addr, Integer val) {
+        datos.add(addr, val);
+    }
+
+    @Override
+    public Integer getAddr(Integer addr) {
+        return datos.get(addr);
+    }
 
 }
